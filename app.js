e385b04c39651e6a45e36c37469bf409226d4ce0
Cristian Caro <99687046+Cristian20044121@users.noreply.github.com>
2023-03-30 08:56:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const boton1= document.getElementById('boton-1');
+boton1.addEventListener('click', function() {
+    document.querySelector('link').href= 'css/estilos-1.css'
+});
+
+const boton2= document.getElementById('boton-2');
+boton2.addEventListener('click',function(){
+    document.querySelector('link').href='css/estilos-2.css'
+});
